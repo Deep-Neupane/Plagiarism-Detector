5bb11d3dd27290ba2f955a6cb775ac3e15195dba
SodiumZincate <144541256+SodiumZincate@users.noreply.github.com>
2026-06-23 21:20:51 +0545

    Update history.js

--- backend/src/routes/history.js
+++ backend/src/routes/history.js
@@ -1,1 +1,9 @@
 ///history route
+const express = require('express');
+const {getAllMatches}=require('../controllers/historyController');
+
+const router = express.Router();
+
+router.get('/',getAllMatches);
+
+module.exports = router;
